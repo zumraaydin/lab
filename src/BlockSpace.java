f85aaff77dab892935e33fe0f6acadff23b382f3
MERHABA <zumra12052005@gmail.com>
2025-10-10 15:58:56 +0300

    init commit

---
+++ src/BlockSpace.java
@@ -0,0 +1,15 @@
+public class BlockSpace {
+    public static void main(String[] args) {
+        int outerx = 5;
+        System.out.println("before block x= " +outerx);
+        if (outerx<0){
+            int innerY=15;
+            System.out.println("ınside block outerx " +outerx);
+            System.out.println("ınside block outerx "+innerY);
+
+
+        }
+
+
+    }
+}
